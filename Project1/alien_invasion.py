34574c9a7aecccfc78df9ea97f1846ba3db8d4c0
Giang Duy Nguyen <giang.nguyen@niteco.se>
2018-06-12 10:47:06 +0700

    Checking to See Whether an Alien Has Hit the Edge - p278

--- Project1/alien_invasion.py
+++ Project1/alien_invasion.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from Project1 import settings
-from Project1 import ship
+from Project1 import ship as sh
 from Project1 import alien
 from Project1 import game_functions as gf
 from pygame.sprite import Group
@@ -12,18 +12,19 @@ def run_game():
     ai_settings = settings.Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship_1 = ship.Ship(ai_settings, screen)
-    alien1 = alien.Alien(ai_settings,screen)
+    ship = sh.Ship(ai_settings, screen)
+    alien1 = alien.Alien(ai_settings, screen)
     bullets = Group()
     aliens = Group()
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
-        gf.check_events(ai_settings, screen, ship_1, bullets)
-        ship_1.update()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship_1, aliens, bullets)
+        gf.update_aliens(aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
